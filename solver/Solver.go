adb1bdaeea37f889d66bebff1bafa1b20ee52d68
Anya SCHUKIN <aschukin@student.42.fr>
2019-05-25 14:41:22 +0200

    pretty sure swapEmpty is garbage

--- solver/Solver.go
+++ solver/Solver.go
@@ -2,11 +2,48 @@ package solver
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-func swapEmpty(puzzle, size) {
-	idx := puzzle[0]
+func randomChoice(Poss []int) int {
+	rand.Seed(time.Now().Unix())
+	swi := rand.Intn(len(Poss))
+	return swi
+}
 
+func findIndexSlice(slice []int, value int) int {
+	for p, v := range slice {
+		if value == v {
+			return p
+		}
+	}
+	return -1
+}
+
+func swapEmpty(puzzle []int, size int) {
+	fmt.Print(puzzle)
+	idx := findIndexSlice(puzzle, 0)
+	fmt.Print(idx)
+	fmt.Println("\n")
+	var Poss []int
+	if idx%size > 0 {
+		Poss = append(Poss, idx-1)
+	}
+	if idx%size < size-1 {
+		Poss = append(Poss, idx+1)
+	}
+	if idx/size > 0 {
+		Poss = append(Poss, idx-size)
+	}
+	if idx/size < size-1 {
+		Poss = append(Poss, idx+size)
+	}
+	//fmt.Print(Poss)
+	swi := randomChoice(Poss)
+	//fmt.Print(swi)
+	puzzle[idx] = puzzle[swi]
+	puzzle[swi] = 0
 }
 
 func makeGoal(size int) []int {
@@ -21,11 +58,14 @@ func makeGoal(size int) []int {
 		puzzle[i] = cur
 		cur++
 	}
-	return (puzzle)
+	return puzzle
 }
 
 // if I comment this
 func MakePuzzle(size int, solve bool, iterations int) {
 	p := makeGoal(size)
+	for i := 1; i <= iterations; i++ {
+		swapEmpty(p, size)
+	}
 	fmt.Print(p)
 }
