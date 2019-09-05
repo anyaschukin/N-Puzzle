99bece19df2dc323f8f10a353480a5dd9b8ece78
A <A@As-MacBook-Air.local>
2019-09-05 14:41:54 +0200

    WIP solvable starting to work

--- resources/performance_test.sh
+++ resources/performance_test.sh
@@ -1,17 +1,17 @@
-echo "\x1b[1mLaunching N-Puzzle performance test ...\x1B[0m\n"
+echo "\x1b[1mLaunching N-Puzzle performance test \x1B[0m...\n"
 ## if fail
 ## echo "Usage: '''go build''' to build the binary 'n-puzzle'. then ./performance_test.sh"
 
 # 3
 echo "\x1b[1mSize - 3\x1B[0m"
 
-# Unsolvable
+####  -- Unsolvable --  ####
 case=10
 count=0
 u=0
 while [ $count -lt $case ]
-do	
-	echo "\r-- $count/$case --\c"
+do
+	echo ".\c"
 	output=$(python generator.py -u 3 >> rm_me.txt; ../n-puzzle rm_me.txt)
 	unsolvable="This puzzle is unsolvable."
 	if [ "$output"="$unsolvable" ]
@@ -19,6 +19,7 @@ do
 		u=$(($u + 1))
 	fi	
 	count=$(($count + 1))
+	$(rm rm_me.txt)
 done
 if [ "$u" -lt "$count" ]
 then
@@ -26,21 +27,38 @@ then
 else
 	echo "\x1b[32m"
 fi
-echo "\nUnsolvable correctly identified: $u/$count\x1b[0m"
+echo "Unsolvable correctly identified: $u/$count\x1b[0m"
 
-# Solvable
+####  -- Solvable --  ####
 case=10
 count=0
 solved=0
-#while [ $count -lt $(expr $case + 1) ]
-#do
-#	echo "\r-- $count/$case --\c"
-#	echo "\n"
-#	time=$(python generator.py -u 5 >> rm_me.txt; ../n-puzzle rm_me.txt)
-#	count=$(($count + 1))
-#done
+while [ $count -lt $case ]
+do
+	echo ".\c"
+	solvable=$(python generator.py -s 3 >> rm_me.txt; ../n-puzzle rm_me.txt )
+#	end=$(($solvable))
+#	echo "$end"
+#	echo "$solvable"
+	finished=" You've finished n-puzzle!"
+	if [ "$end"="$finished" ]
+	then
+		solved=$(($solved + 1))
+	fi
+	count=$(($count + 1))
+	$(rm rm_me.txt)
+done
 
-echo "Solvable correctly solved: " ## out of 10
+#echo "$time"
+
+
+if [ "$u" -lt "$count" ]
+then
+	echo "\x1b[31m"
+else
+	echo "\x1b[32m"
+fi
+echo "Solvable correctly solved: $solved/$count\x1b[0m"
 echo "Average solve time: "
 echo "Min solve time: "
 echo "Max solve time: "
