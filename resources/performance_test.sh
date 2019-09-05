e2e50c89db03cfbcb2fa950a9c41e7307860c975
A <A@As-MacBook-Air.local>
2019-09-05 12:51:05 +0200

    unsolvable performance test working at basic level, moving on to solvable

--- resources/performance_test.sh
+++ resources/performance_test.sh
@@ -7,16 +7,20 @@ echo "\x1b[1mSize - 3\x1B[0m"
 case=10
 count=1
 u=0
-#while [ $count -lt $(expr $case + 1) ]
-#do	
+while [ $count -lt $(expr $case + 1) ]
+do	
 	echo "\r-- $count/$case --\c"
 	output=$(python generator.py -u 5 >> rm_me.txt; ../n-puzzle rm_me.txt)
-	echo "$output"
-#	if [ $output="unsolvable.\n" ]
-#	then
-#		u=$(($u + 1))		
-#	fi	
-#done
+##	echo "$output"
+	check="This puzzle is unsolvable."
+	if [ "$output"="$check" ]
+	then
+		u=$(($u + 1))
+	fi	
+	count=$(($count + 1))
+done
+echo "\nUnsolvable correctly identified: $u/10" ## out of 10
+##echo "$u"	
 
 #while [ $count -lt $(expr $case + 1) ]
 #do
